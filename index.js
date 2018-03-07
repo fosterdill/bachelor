2d7cd049cc8fe18a05294462a988a986dc3f2095
Dylan Foster <fosterdill@gmail.com>
2018-03-06 21:17:02 -0800

    rename / remove stuf

--- index.js
+++ index.js
@@ -1,42 +1,86 @@
-const state = {};
-const ClickIntent = 'ClickIntent';
+const GetPayload = 'GetPayload';
+const ViewPayload = 'ViewPayload';
 
-const stateReducer = (state) => {
+// state denormalized from DOM and API
+// actions by users are described by intents
+// a stateReducer is a function that returns object describing element
+// intentreducer turns intent into updated state
+
+class State extends EventEmitter {
+  get() {
+    return this.state;
+  }
+
+  update(newState) {
+    this.state = newState;
+
+    this.emit('change');
+  }
+}
+
+const state = new State();
+
+const handlerToIntentReducer = (handler, thing) => {
+  return handler(thing, state);
+};
+
+const stateSetter = (handlerToIntentReducer, thing) => {
+  const objectOrPromise = intentReducer(handlerToIntentReducer(thing), thing);
+
+  if (objectOrPromise instanceof Promise) { objectOrPromise.then((object) => { state.update(object); }); } else {
+    state.update(objectOrPromise);
+  }
+};
+
+const digestReturnedIntent = (intentCreator) =>
+  stateSetter.bind(null, handlerToIntentReducer.bind(null, intentCreator));
+
+const stateReducer = () => {
   return {
     type: 'div',
-    props: {
-      onclick: () => ClickIntent,
+    listeners: {
+      click: (e) => ViewPayload,
     },
+    innerHTML: state.get().something,
   };
 };
 
-const intentReducer = (intent) => {
-  switch (intent) {
-    case ClickIntent:
-      return { clicked: true };
+const viewPayload = () => {
+  digestReturnedIntent(() => GetPayload)();
 
+  return { something: 'loading' };
+};
+
+
+const getPayload = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts/1/')
+    .then((object) => object.json())
+    .then((object) => Promise.resolve({ something: JSON.stringify(object) }));
+};
+
+const intentReducer = (intent, object) => {
+  switch (intent) {
+    case ViewPayload: return viewPayload();
+    case GetPayload: return getPayload();
     default:
       return {};
   }
 };
 
-const handlerToIntentReducer = (handler, event) => {
-  return handler(event, state);
-};
-
-const stateSetter = (handlerToIntentReducer, event) => {
-  state = intentReducer(handlerToIntentReducer(event));
-};
-
 const elementObjectReducer = (elementObject) => {
-  return Object.keys(elementObject.props)
-    .reduce((element, prop) => (
-      elementObject.setAttribute(handlerToIntentReducer.bind(null, handler))
-    ), document.createElement(elementObject.type));
+  return Object.keys(elementObject.listeners)
+    .reduce((element, listener) => {
+      element.addEventListener(listener, digestReturnedIntent(elementObject.listeners[listener]));
+      element.innerHTML = elementObject.innerHTML;
+
+      return element;
+    }, document.createElement(elementObject.type));
 };
 
-const renderToBody = () => {
-  document.body.append(elementObjectReducer(stateReducer(state)));
+const main = () => {
+  document.body.innerHTML = '';
+  document.body.append(elementObjectReducer(stateReducer()));
 };
 
-state = renderToBody();
+state.addListener('change', main);
+state.update({ something: 'hi' });
